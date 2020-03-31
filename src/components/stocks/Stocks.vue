64ac590b2d47e642913c3471303f36d7fc58de88
Nicodemus <nicodemus.maurits@gmail.com>
2020-03-31 15:21:56 +0200

    Initialize Vuex Store for stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+    <app-stock
+      v-for="stock in stocks"
+      :key="stock.id"
+      :stock="stock"
+    ></app-stock>
   </div>
 </template>
 
@@ -8,18 +12,13 @@
 import Stock from "./Stock.vue";
 
 export default {
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: "BMW", price: 110 },
-        { id: 2, name: "Google", price: 200 },
-        { id: 3, name: "Apple", price: 175 },
-        { id: 4, name: "Twitter", price: 8 }
-      ]
-    };
-  },
   components: {
     appStock: Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks;
+    }
   }
 };
 </script>
