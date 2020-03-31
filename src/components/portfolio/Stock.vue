783a8fce971b0eed556694eb8b867b9600604592
Nicodemus <nicodemus.maurits@gmail.com>
2020-03-31 16:42:02 +0200

    Map action to avoid conflict with local methods

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -43,14 +43,17 @@ export default {
     };
   },
   methods: {
-    ...mapActions(["sellStock"]),
+    ...mapActions({
+      placeSellOrder: "sellStock"
+    }),
     sellStock() {
       const order = {
         stockId: this.stock.id,
         stockPrice: this.stock.price,
         quantity: this.quantity
       };
-      this.sellStock();
+      this.placeSellOrder(order);
+      this.quantity = 0;
     }
   }
 };
